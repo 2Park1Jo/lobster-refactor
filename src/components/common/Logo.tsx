1becffa8d6618297f35ed7f26450756c227b1bc2
DaeWon9 <qkreodnjs97@naver.com>
2024-01-29 20:09:11 +0900

    initial setting

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,7 @@
+import loginStyles from '../../styles/login/loginPage.module.scss'
+
+const Logo = () => {
+  return <div className={loginStyles.login_logo}></div>
+}
+
+export default Logo
---
+++ src/pages/WorkspaceBanner.tsx
@@ -0,0 +1,5 @@
+const WorkspaceBanner = () => {
+  return <div></div>
+}
+
+export default WorkspaceBanner
