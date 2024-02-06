eb3309728bab13a40354eefca3dcd269436664ec
DaeWon9 <qkreodnjs97@naver.com>
2024-02-06 20:44:34 +0900

    [FIX] KEYS

--- src/constants/workspace/index.ts
+++ src/constants/workspace/index.ts
@@ -1,5 +1,3 @@
-export const WORKSPACE_ADD_KEY = '0'
-
-export const WORKSPACE_EMPTY_CARD_KEY = '0'
-
+export const WORKSPACE_ADD_CARD_KEY = '0'
+export const WORKSPACE_EMPTY_CARD_KEY = '-1'
 export const RANDOM_IMAGE_URL = 'https://picsum.photos/500/300?img='
