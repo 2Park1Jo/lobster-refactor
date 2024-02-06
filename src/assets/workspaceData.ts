cc99ef21c9adfa2e8463c909c7d1dc9fde06741e
DaeWon9 <qkreodnjs97@naver.com>
2024-02-06 20:43:33 +0900

    [FIX] workspace interface

--- src/assets/workspaceData.ts
+++ src/assets/workspaceData.ts
@@ -1,47 +1,48 @@
-interface Workspace {
-  workspaceId: string
-  workspaceName: string
-  workspaceGoal: string
-  workspaceDeadline: string
-}
+import { Workspace } from '../interfaces/workspace/Workspace'
 
 const workspaceData: Workspace[] = [
-  {
-    workspaceId: '1',
-    workspaceName: '오픈소스과제',
-    workspaceGoal: '오픈소스 프로젝트 제작',
-    workspaceDeadline: '2022-10-30',
-  },
-  {
-    workspaceId: '2',
-    workspaceName: '데이터베이스 과제방',
-    workspaceGoal: '데이터베이스 발표 PPT 완성',
-    workspaceDeadline: '2022-10-30',
-  },
-  {
-    workspaceId: '3',
-    workspaceName: '대원이와 아이들 과제',
-    workspaceGoal: '공모전 상타기',
-    workspaceDeadline: '2023-01-30',
-  },
-  {
-    workspaceId: '4',
-    workspaceName: '형준이는 치타일까',
-    workspaceGoal: '형준이에 대한 연구',
-    workspaceDeadline: '2022-10-30',
-  },
-  {
-    workspaceId: '5',
-    workspaceName: '엔샵 동아리방',
-    workspaceGoal: '스터디 완수',
-    workspaceDeadline: '2022-10-30',
-  },
-  {
-    workspaceId: '6',
-    workspaceName: '세종대 축제준비방',
-    workspaceGoal: '축제 성공적으로 끝내기',
-    workspaceDeadline: '2022-10-30',
-  },
+  // {
+  //   id: '1',
+  //   name: '오픈소스과제',
+  //   goal: '오픈소스 프로젝트 제작',
+  //   deadline: '2022-10-30',
+  //   alertCount: 0,
+  // },
+  // {
+  //   id: '2',
+  //   name: '데이터베이스 과제방',
+  //   goal: '데이터베이스 발표 PPT 완성',
+  //   deadline: '2022-10-30',
+  //   alertCount: 0,
+  // },
+  // {
+  //   id: '3',
+  //   name: '대원이와 아이들 과제',
+  //   goal: '공모전 상타기',
+  //   deadline: '2023-01-30',
+  //   alertCount: 0,
+  // },
+  // {
+  //   id: '4',
+  //   name: '형준이는 치타일까',
+  //   goal: '형준이에 대한 연구',
+  //   deadline: '2022-10-30',
+  //   alertCount: 0,
+  // },
+  // {
+  //   id: '5',
+  //   name: '엔샵 동아리방',
+  //   goal: '스터디 완수',
+  //   deadline: '2022-10-30',
+  //   alertCount: 0,
+  // },
+  // {
+  //   id: '6',
+  //   name: '세종대 축제준비방',
+  //   goal: '축제 성공적으로 끝내기',
+  //   deadline: '2022-10-30',
+  //   alertCount: 0,
+  // },
 ]
 
 export function getWorkspaceData(): Workspace[] {
